00399eb39fd38c1fb2d23ba037a9ce9f71277c6d
ezequiel-7 <ezekrom007@gmail.com>
2021-03-05 12:00:20 -0300

    feature/formulario-cliente-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormularioClienteComponent } from './formulario-cliente/formulario-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioClienteComponent
   ],
   imports: [
     BrowserModule
